f9838c97f68d1523a3593c73beebe90a050d8d24
Iago Virgílio <iagovirgilio@gmail.com>
2025-02-06 19:46:02 -0300

    Criar letas minúsculas em `font.h`

---
+++ DisplayC.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+
+int main() {
+    // Inicialização do I2C
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+    
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_send_data(&ssd);
+
+    // Limpa o display
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+    
+    bool cor = true;
+    while (true) {
+        cor = !cor;
+        ssd1306_fill(&ssd, !cor);
+        ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor);
+        
+        // Agora suporta letras minúsculas
+        ssd1306_draw_string(&ssd, "Cepedi Tic37", 8, 10);
+        ssd1306_draw_string(&ssd, "Embarcatech", 20, 30);
+        ssd1306_draw_string(&ssd, "Prof Wilton", 15, 48);
+        
+        ssd1306_send_data(&ssd);
+        sleep_ms(1000);
+    }
+}
